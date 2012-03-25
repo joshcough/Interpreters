fd59ccb0742abbccd4cc1ee090dbe2214b64b920
Josh Cough <joshcough@gmail.com>
2012-03-25 12:38:38 -0400

    cleanup

--- src/main/scala/typecheck/TypeCheckerWithExplicitTypesV2MonadicReader.scala
+++ src/main/scala/typecheck/TypeCheckerWithExplicitTypesV2MonadicReader.scala
@@ -45,47 +45,49 @@ object TypeCheckerWithExplicitTypesV2MonadicReader {
     //  tv => "if" -> (TyLam(boolT, TyLam(tv, TyLam(tv, tv))), Set())
   )
 
-  // promote an either to a Kleisli, ignoring the environment.
-  def liftK[T,U](e:Either[String, U])= kleisli[V, T, U]((env: T) => e)
+  // the real type check function, which works with the type environment.
 
   type V[T] = Either[String, T]
+  
+  def find(s:String, env:TypeEnv): Either[String, Type] =
+    env.find(_._1 == s).map(p => Right(p._2)).getOrElse(Left("not found: " + s))
+
+  // liftK essentially promotes an Either to a Kleisli.
+  def liftK[T,U](e:Either[String, U])= kleisli[V, T, U]((env: T) => e)
 
   // TODO: i should be able to import this from somewhere in scalaz
   def local[F[_], A, R](f: (R) => R)(fa: Kleisli[F, R, A]): Kleisli[F, R, A] =
     Kleisli[F, R, A](r => fa.run(f(r)))
 
-  def typeError(s:String) = Left(s)
   def success(t:Type) = Right(t)
+  def typeError(msg:String) = Left(msg)
 
-  def find(s:String, env:TypeEnv): Either[String, Type] =
-    env.find(_._1 == s).map(p => Right(p._2)).getOrElse(Left("not found: " + s))
+  def compare(t1: Type, t2: Type, resultType: Type, errorMsg: String) =
+    if(t1 == t2) success(resultType) else typeError(errorMsg)
 
-  def checkEqual(t1:Type, t2: Type, resultType: Type, errorMessage: String) =
-    if(t1 == t2) success(resultType) else typeError(errorMessage)
-
-  // the real type check function, which works with the type environment.
   def typeCheck(expr: Exp): ReaderT[V, TypeEnv, Type] = expr match {
-    case Lit(v) =>liftK(success(litToTy(v)))
-    case Id(x) => for (env <- ask[V, TypeEnv]; res <-liftK(find(x, env))) yield res
+    case Lit(v) => liftK(success(litToTy(v)))
+    case Id(x)  => for { env <- ask[V, TypeEnv]; res <- liftK(find(x, env)) } yield res
+    // make sure the first branch is a boolean and then
+    // make sure the second and third branches have the same type
     case If(tst, texp, fexp) => for {
-      // make sure the first branch is a boolean
-      t <- typeCheck(tst)
-      _ <-liftK(checkEqual(t, boolT, resultType=boolT, "if required bool in test position, but got: " + t))
-      // make sure the second and third branches have the same type
-      lt <- typeCheck(texp)
-      rt <- typeCheck(fexp)
-      result <-liftK(checkEqual(lt, rt, resultType = lt, "if branches not the same type, got: " + (lt, rt)))
-    } yield result
+      t   <- typeCheck(tst)
+      _   <- liftK(compare(t, boolT, boolT, "if required bool in test position, but got: " + t))
+      lt  <- typeCheck(texp)
+      rt  <- typeCheck(fexp)
+      res <- liftK(compare(lt, rt, lt, "if branches not the same type, got: " + (lt, rt)))
+    } yield res
     case Fun(arg, argType, body) => for {
       t <- local((env:TypeEnv) => env + (arg -> argType))(typeCheck(body))
     } yield TyLam(argType, t)
     case App(operator, operand) => for {
-      opType <- typeCheck(operator)
-      operandType <- typeCheck(operand)
-      res <-liftK(opType match {
-        case TyLam(argType, resultType) => checkEqual(argType, operandType, resultType,
-          "function expected arg of type: " + argType + ", but got: " + operandType)
-        case _ => typeError("function application expected function, but got: " + opType)
+      operatorType <- typeCheck(operator)
+      operandType  <- typeCheck(operand)
+      res <- liftK(operatorType match {
+        case TyLam(argType, resultType) =>
+          compare(argType, operandType, resultType,
+            "function expected arg of type: " + argType + ", but got: " + operandType)
+        case _ => typeError("function application expected function, but got: " + operatorType)
       })
     } yield res
   }
