6778f1921a10ab99059072b4df486a12b3dd8a31
Josh Cough <joshcough@gmail.com>
2012-03-26 17:06:58 -0400

    refactored out all the common code from the HM type checkers.

--- src/main/scala/typecheck/TypeCheckerWithInference.scala
+++ src/main/scala/typecheck/TypeCheckerWithInference.scala
@@ -33,51 +33,7 @@ package typecheck
 
 object TypeCheckerWithInference {
 
-  // AST
-
-  trait Literal
-  case class Num(i:Int) extends Literal
-  case class Bool(b:Boolean) extends Literal
-
-  trait Exp
-  case class Var(name:String) extends Exp
-  case class Lam(arg: String, body: Exp) extends Exp
-  case class App(f:Exp, arg: Exp) extends Exp
-  case class Lit(l:Literal) extends Exp
-  //  Let      of string * Exp * Exp   // local definition
-
-  // Type Tree
-
-  trait Type
-  case class TyLam(f:Type, arg:Type) extends Type
-  case class TyVar(name:String) extends Type
-  case class TyCon(name:String, args: List[Type]) extends Type
-
-  // Subtitutions
-
-  type Subst = Map[String, Type]
-  def extend(v:String, t:Type, subs: Subst): Subst = subs + (v -> t)
-  def lookup(v:String, subs:Subst) = subs.getOrElse(v, TyVar(v))
-  def subs(t:Type, s:Subst): Type = t match {
-    case TyVar(n) =>
-      val tp = lookup(n, s)
-      if(t == tp) tp else subs(tp, s)
-    case TyLam(a, r) => TyLam(subs(a, s), subs(r, s))
-    case TyCon(name, tyArgs) => TyCon(name, tyArgs.map(subs(_, s)))
-  }
-
-  // Environments
-
-  type TyScheme = (Type, Set[String])
-  type Env = Map[String, TyScheme]
-  // TODO: TyScheme is used for Let, which I haven't done yet.
-  // This implementation could just have type Env = Map[String, Type]
-
-  def getTVarsOfType(t:Type): Set[String] = t match {
-    case TyVar(n) => Set(n)
-    case TyLam(t1, t2) => getTVarsOfType(t1) ++ getTVarsOfType(t2)
-    case TyCon(_, args) => args.flatMap(t => getTVarsOfType(t)).toSet
-  }
+  import TypeCheckerWithInferenceAST._
 
   // Unification
 
@@ -119,24 +75,6 @@ object TypeCheckerWithInference {
   var ts = Iterator.from(0)
   def resetTypCounter() = ts = Iterator.from(0)
   def newTypVar() = TyVar("t" + ts.next())
-
-  val numCon = TyCon("Num", Nil)
-  val boolCon = TyCon("Bool", Nil)
-
-  def litToTy(l:Literal): Type = l match {
-    case Num(_) => numCon
-    case Bool(_) => boolCon
-  }
-
-  val predef: Env = Map(
-    "+" ->   (TyLam(numCon, TyLam(numCon, numCon)), Set()),
-    "-" ->   (TyLam(numCon, TyLam(numCon, numCon)), Set()),
-    "==" ->  (TyLam(numCon, TyLam(numCon, boolCon)), Set()),
-    "and" -> (TyLam(boolCon, TyLam(boolCon, boolCon)), Set()),
-    "or" ->  (TyLam(boolCon, TyLam(boolCon, boolCon)), Set())
-    // TODO: how do we do if? is it like this?
-    //  tv => "if" -> (TyLam(boolCon, TyLam(tv, TyLam(tv, tv))), Set())
-  )
 
   // the top level type check function
   def typeCheck(exp: Exp): Type = {
--- src/main/scala/typecheck/TypeCheckerWithInference_MonadTransformers.scala
+++ src/main/scala/typecheck/TypeCheckerWithInference_MonadTransformers.scala
@@ -1,88 +1,19 @@
 package typecheck
 
-import scalaz.State._
-import scalaz.std.list.{listInstance => listTraverse}
-import scalaz.{State, EitherT}
-import EitherT._
-import scalaz.syntax.monad._
-
 /**
  * Note: A lot of this code is a port from:
  *   http://fsharpcode.blogspot.com/2010/08/hindley-milner-type-inference-sample.html
- *
- * TODO: comment is out of date.
- *
- * I'm writing this to explore type checking with type inference in Scala.
- *
- * To do this, I'm starting with a very simple language that allows
- * only one top level expression.
- *
- * The language has three types: Int, Bool, and Arrow (List T) T
- * and corresponding literals for those types (shown in examples below).
- *
- * There are four built-in functions:
- *   if  : (Bool -> T -> T) -> T
- *   add : (Int  -> Int) -> Int
- *   sub : (Int  -> Int) -> Int
- *   eql : (Int  -> Int) -> Bool
- *
- * The user is not required to put explicit type declarations on function arguments,
- * or the function return type. These types will be inferred by the type checker.
- *
  */
-
-/**
- *  TODO: these add let...
- * What happens here?
- * let id x = x in if (id true) (id 6) (id 7)
- */
-
 object TypeCheckerWithInference_MonadTransformers {
 
-  // AST
+  import TypeCheckerWithInferenceAST._
 
-  trait Literal
-  case class Num(i: Int) extends Literal
-  case class Bool(b: Boolean) extends Literal
-
-  trait Exp
-  case class Var(name: String) extends Exp
-  case class Lam(arg: String, body: Exp) extends Exp
-  case class App(f: Exp, arg: Exp) extends Exp
-  case class Lit(l: Literal) extends Exp
-  //  Let      of string * Exp * Exp   // local definition
-
-  // Type Tree
-
-  trait Type
-  case class TyLam(f: Type, arg: Type) extends Type
-  case class TyVar(name: String) extends Type
-  case class TyCon(name: String, args: List[Type]) extends Type
-
-  // Subtitutions
-
-  type Subst = Map[String, Type]
-  def extend(v: String, t: Type, subs: Subst): Subst = subs + (v -> t)
-  def lookup(v: String, subs: Subst): Type = subs.getOrElse(v, TyVar(v))
-  def subs(t: Type, s: Subst): Type = t match {
-    case TyVar(n) =>
-      val tp = lookup(n, s)
-      if (t == tp) tp else subs(tp, s)
-    case TyLam(a, r) => TyLam(subs(a, s), subs(r, s))
-    case TyCon(name, tyArgs) => TyCon(name, tyArgs.map(subs(_, s)))
-  }
-
-  // Environments
-
-  type TyScheme = (Type, Set[String])
-  type Env = Map[String, TyScheme]
-  // TODO: TyScheme is used for Let, which I haven't done yet.
-  // This implementation could just have type Env = Map[String, Type]
-  def getTVarsOfType(t: Type): Set[String] = t match {
-    case TyVar(n) => Set(n)
-    case TyLam(t1, t2) => getTVarsOfType(t1) ++ getTVarsOfType(t2)
-    case TyCon(_, args) => args.flatMap(t => getTVarsOfType(t)).toSet
-  }
+  import scalaz.State._
+  import scalaz.std.list.{listInstance => listTraverse}
+  import scalaz.{StateT, State, EitherT}
+  import EitherT._
+  import scalaz.std.either._
+  import scalaz.syntax.monad._
 
   // Unification
 
@@ -145,24 +76,6 @@ object TypeCheckerWithInference_MonadTransformers {
       } yield res
     }
   }
-
-  val numCon  = TyCon("Num", Nil)
-  val boolCon = TyCon("Bool", Nil)
-
-  def litToTy(l: Literal): Type = l match {
-    case Num(_) => numCon
-    case Bool(_) => boolCon
-  }
-
-  val predef: Env = Map(
-    "+" ->(TyLam(numCon, TyLam(numCon, numCon)), Set()),
-    "-" ->(TyLam(numCon, TyLam(numCon, numCon)), Set()),
-    "==" ->(TyLam(numCon, TyLam(numCon, boolCon)), Set()),
-    "and" ->(TyLam(boolCon, TyLam(boolCon, boolCon)), Set()),
-    "or" ->(TyLam(boolCon, TyLam(boolCon, boolCon)), Set())
-    // TODO: how do we do if? is it like this?
-    //  tv => "if" -> (TyLam(boolCon, TyLam(tv, TyLam(tv, tv))), Set())
-  )
 
   // rename all the type variables starting from t0
   // this cleans things up a bit as far as presentation goes.
